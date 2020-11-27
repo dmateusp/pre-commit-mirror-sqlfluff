dd92c8ad4eecfdf9d5a2fac00bd96b71b9833241
pre-commit <pre-commit@example.com>
2020-11-27 08:44:35 +0000

    Mirror: 0.4.0a1

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['sqlfluff==0.3.6'],
+    install_requires=['sqlfluff==0.4.0a1'],
 )
